ed6592e2a9f46dbd88e3a46232d88f7a39bbb932
alvinskie <azeus9289@gmail.com>
2026-04-14 18:48:26 +0700

    Update properti

--- ManajemenPerumahan/src/perumahan/model/Properti.java
+++ ManajemenPerumahan/src/perumahan/model/Properti.java
@@ -1,62 +1,56 @@
 package perumahan.model;
+
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public abstract class Properti {
+public class Properti {
+    protected String kode;
+    protected String nama;
+    protected double harga;
+    protected String lokasi;
 
-    private String id;
-    private String nama;
-    private double harga;
-    private String lokasi;
-    private boolean terjual;
-    
-    private String namaPembeli;
-    private String alamatPembeli;
-    private String noHpPembeli;
-    private String tanggalPembelian;
-    private String metodePembayaran; 
+    protected boolean terjual = false;
+    protected String namaPembeli = "-";
+    protected String alamatPembeli;
+    protected String noHpPembeli;
+    protected String tanggalPembelian;
+    protected String metodePembayaran;
 
-    public Properti(String id, String nama, double harga, String lokasi) {
-        this.id = id;
+    public Properti(String kode, String nama, double harga, String lokasi) {
+        this.kode = kode;
         this.nama = nama;
         this.harga = harga;
         this.lokasi = lokasi;
-        this.terjual = false;
-        
-        // Nilai default
-        this.namaPembeli = "-";
-        this.alamatPembeli = "-";
-        this.noHpPembeli = "-";
-        this.tanggalPembelian = "-";
-        this.metodePembayaran = "-"; 
     }
+    public String getKode() { return kode; }
+    public String getLokasi() { return lokasi; }
 
-    public abstract void tampilkanInfo();
-
-    public String getId() { return id; }
     public String getNama() { return nama; }
     public double getHarga() { return harga; }
-    public String getLokasi() { return lokasi; }
+    public String getHargaFormat() { return formatRupiah(harga); }
+
     public boolean isTerjual() { return terjual; }
-    public void setTerjual(boolean status) { this.terjual = status; }
-    
+    public void setTerjual(boolean terjual) { this.terjual = terjual; }
+
     public String getNamaPembeli() { return namaPembeli; }
     public void setNamaPembeli(String namaPembeli) { this.namaPembeli = namaPembeli; }
-    
+
+    public void setAlamatPembeli(String a) { this.alamatPembeli = a; }
+    public void setNoHpPembeli(String n) { this.noHpPembeli = n; }
+    public void setTanggalPembelian(String t) { this.tanggalPembelian = t; }
+    public void setMetodePembayaran(String m) { this.metodePembayaran = m; }
+
     public String getAlamatPembeli() { return alamatPembeli; }
-    public void setAlamatPembeli(String alamat) { this.alamatPembeli = alamat; }
-    
     public String getNoHpPembeli() { return noHpPembeli; }
-    public void setNoHpPembeli(String noHp) { this.noHpPembeli = noHp; }
-    
     public String getTanggalPembelian() { return tanggalPembelian; }
-    public void setTanggalPembelian(String tanggal) { this.tanggalPembelian = tanggal; }
-
     public String getMetodePembayaran() { return metodePembayaran; }
-    public void setMetodePembayaran(String metode) { this.metodePembayaran = metode; }
 
-    public String formatRupiah(double nominal) {
-        NumberFormat nf = NumberFormat.getInstance(Locale.forLanguageTag("id-ID"));
-        return "Rp " + nf.format(nominal);
+    public String formatRupiah(double angka) {
+        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("id", "ID"));
+        return nf.format(angka);
+    }
+
+    public void tampilkanInfo() {
+        System.out.println(kode + " | " + nama + " | " + formatRupiah(harga) + " | " + lokasi);
     }
 }
