1540199243e1d553e5137681de2267b1835df82c
indasyifad <indasyifad@gmail.com>
2026-04-22 10:38:18 +0700

    Update apartemen.java

--- ManajemenPerumahan/src/perumahan/model/Apartemen.java
+++ ManajemenPerumahan/src/perumahan/model/Apartemen.java
@@ -1,21 +1,15 @@
 package perumahan.model;
 
 public class Apartemen extends Properti {
-
     private int lantai;
 
-    public Apartemen(String id, String nama, double harga, String lokasi, int lantai) {
-        super(id, nama, harga, lokasi);
+    public Apartemen(String kode, String nama, double harga, String lokasi, int lantai) {
+        super(kode, nama, harga, lokasi);
         this.lantai = lantai;
     }
 
     @Override
     public void tampilkanInfo() {
-        String status = isTerjual() ? "TERJUAL" : "TERSEDIA";
-        String strDetail = "-";
-        String strUkuran = String.format("Lt. %d", lantai);
-        
-        System.out.printf("%-5s | %-10s | %-20s | %-10s | %-10s | %-16s | %-10s | %-15s\n",
-                getId(), "Apartemen", getNama(), strDetail, strUkuran, formatRupiah(getHarga()), status, getNamaPembeli());
+        System.out.println(kode + " | Apartemen | " + nama + " | Lt " + lantai + " | " + formatRupiah(harga));
     }
 }
